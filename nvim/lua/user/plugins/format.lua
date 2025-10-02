5e4d495d97845481ae2d29587aa45190d84c6fb9
mirkowang <mirkowang@hotmail.com>
2025-10-02 11:48:38 +0800

    formalize format.lua

--- nvim/lua/user/plugins/format.lua
+++ nvim/lua/user/plugins/format.lua
@@ -5,20 +5,24 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				-- pip3 install isort black
+				-- current: managed by mason
 				lua = { "stylua" },
 
 				-- pip3 install ruff
+				-- current: managed by mason
 				python = { "ruff_format" },
 
 				-- gem install rubocop
-				-- ruby = { "rubocop" },
+				-- current: managed by mason
+				ruby = { "rubocop" },
 
 				-- gem install erb-formatter
+				-- current: managed by mason
 				eruby = { "erb_format" },
 			},
 
 			format_on_save = {
-				timeout_ms = 200,
+				timeout_ms = 2000,
 				lsp_format = "fallback",
 			},
 		})
